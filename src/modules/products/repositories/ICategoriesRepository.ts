f644bbdfdd2fd6e68203cb147d4278d1006f70df
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-05-26 17:59:04 -0300

    (feat): create category

---
+++ src/modules/products/repositories/ICategoriesRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateCategoryDTO } from "../dtos/ICreateCategoryDTO";
+import { Category } from "../infra/typeorm/entities/Category";
+
+interface ICategoriesRepository {
+  create(data: ICreateCategoryDTO): Promise<Category>;
+  findByName(name: string): Promise<Category | undefined>;
+}
+
+export { ICategoriesRepository };
---
+++ src/modules/products/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { ICreateCategoryDTO } from "../../dtos/ICreateCategoryDTO";
+import { Category } from "../../infra/typeorm/entities/Category";
+import { ICategoriesRepository } from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  public async create({
+    name,
+    description,
+  }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+
+  public async findByName(name: string): Promise<Category | undefined> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
